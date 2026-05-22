163f1247762f05a4e1852bc98ce8fce6e03fd8f7
Anas Mahamed <anasmahamed03@gmail.com>
2026-05-22 09:16:52 +0100

    Learnt event handling and drawing functions.

--- main.c
+++ main.c
@@ -6,11 +6,30 @@
 int main(void) {
     InitWindow(800, 600, "My First Game");
     SetTargetFPS(60);
+    int speed = 5;
+    int ballX = 400;
+    int ballY = 300;
 
     while (!WindowShouldClose()) {
+        // Event Handling
+        if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)){
+            ballX += speed;
+        }
+        else if(IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)){
+            ballY -= speed;
+        }
+        else if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)){
+            ballY += speed;
+        }
+        else if(IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)){
+            ballX-= speed;
+        }
+
+        // Updating Positions
+        
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawRectangle(0, 100, 800, 20, PURPLE);
+            DrawCircle(ballX, ballY, 30, PURPLE);
             DrawText("Hello, Raylib!", 300, 280, 20, DARKGRAY);
         EndDrawing();
     }
