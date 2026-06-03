e0c4d175db703b869fc3ca14af7ed572cacd3f1b
Anas Mahamed <anasmahamed03@gmail.com>
2026-06-03 15:35:33 +0100

    Fixed bug in startbuttonpressed and quitbuttonpressed

--- mario_rip_off/main.c
+++ mario_rip_off/main.c
@@ -14,7 +14,8 @@
 
 typedef enum state{
     MENU,
-    GAME
+    GAME, 
+    QUIT
 }state;
 
 typedef struct Button{
@@ -25,7 +26,7 @@ typedef struct Button{
     char colour[50];
 } Button;
 
-void DrawButton(Rectangle startButton){
+void Draw_Start_Button(Rectangle startButton){
     DrawRectangleRounded(startButton, 0.2f, 10, GREEN);
     const char *text = "START";
     int fontsize = 24;
@@ -36,8 +37,18 @@ void DrawButton(Rectangle startButton){
 
     DrawText(text, textX, textY, fontsize, WHITE);
 }
+void Draw_Quit_Button(Rectangle quitbutton){
+    DrawRectangleRounded(quitbutton, 0.2f, 10, RED);
+    const char *text = "Quit";
+    int fontsize = 24;
+    int textWidth = MeasureText(text, fontsize);
 
-bool button_Pressed(Rectangle startButton){
+    int textX = quitbutton.x + (quitbutton.width - textWidth) / 2;
+    int textY = quitbutton.y + (quitbutton.height - fontsize) / 2;
+
+    DrawText(text, textX, textY, fontsize, WHITE);
+}
+bool start_button_Pressed(Rectangle startButton){
     
     
     bool hovering = CheckCollisionPointRec(GetMousePosition(), startButton);
@@ -56,6 +67,35 @@ bool button_Pressed(Rectangle startButton){
         
         return true;
     }
+    else{
+        return false;
+    }
+    
+}
+
+bool quit_button_Pressed(Rectangle quitButton){
+    
+    
+    bool hovering = CheckCollisionPointRec(GetMousePosition(), quitButton);
+    if (hovering) {
+        DrawRectangleRec(quitButton, RED); 
+        DrawText("QUIT", 360, 315, 24, WHITE);
+     } // changes color if hovering
+    else {
+        DrawRectangleRec(quitButton, DARKBLUE); 
+        DrawText("QUIT", 360, 315, 24, WHITE);
+    }
+
+    if(hovering && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) // if buttoned clicked
+    {
+        TraceLog(LOG_INFO, "Quit Pressed");
+        
+        return true;
+    }
+    else{
+        return false;
+    }
+    
 }
 
 
@@ -65,12 +105,13 @@ int main(){
     InitWindow(MAP_COLS * TILE_SIZE, MAP_ROWS * TILE_SIZE, "Pete's Pizza");
     printf("Working directory: %s\n", GetWorkingDirectory());
     SetTargetFPS(60);
-    bool clicked;
-    state state = MENU;
+    bool st_clicked;
+    bool qt_clicked;
+    state game_state = MENU;
 
-    Button startButton = {300, 200, 200, 50, GREEN};
-
-   
+    Rectangle startButton = {300, 200, 200, 50};
+    Rectangle quitButton = {300, 200, 200, 150};
+    
     Texture bg = LoadTexture("C://Users//Anas//Raylib_Game//mario_rip_off//start_background.png");
 
     Texture2D tiles[4];
@@ -98,19 +139,20 @@ int main(){
         BeginDrawing();
             ClearBackground(BLACK);
             DrawTexture(bg, 0, 0, WHITE);
-            DrawButton(startButton);
-            clicked = button_Pressed(startButton);
-            switch (clicked)
-            {
-                case true:
-                    printf("Hello");
-                    state = GAME;
-                    break;
-            
-                default:
-                    break;
+            Draw_Start_Button(startButton);
+            Draw_Quit_Button(quitButton);
+            st_clicked = start_button_Pressed(startButton);
+            qt_clicked = quit_button_Pressed(quitButton);
+            printf("%d", st_clicked);
+            if (st_clicked == true){
+                printf("H");
+                game_state = GAME;
             }
-            if (state == GAME){
+            else if(qt_clicked){
+                game_state = QUIT;
+            }
+            
+            if (game_state == GAME){
                 ClearBackground(BLACK);
                 for (int row = 0; row < MAP_ROWS; row++){
                     for (int col = 0; col < MAP_COLS; col++){
@@ -119,6 +161,9 @@ int main(){
                     }
                 }
                 
+            }
+            else if (game_state == QUIT){
+                CloseWindow();
             }
             
             
